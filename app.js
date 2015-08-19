c218bd641230e5f2c1d1c719f143253e61413323
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-08-18 19:46:46 -0700

    boilerplate

---
+++ app.js
@@ -0,0 +1,14 @@
+// same as `$(document).ready`, it will only run once the document loads
+$(function(){
+  //create our octocat template
+  var octocatTemplate = _.template($('#octocat-template').html());
+  // data is defined in data.js
+  var octocats = data.octocats;
+  // get our template to give us an html string for each octocat
+  octocats.forEach(function(octocat){
+    // give our template some data and get html from it
+    var octocatHtml = octocatTemplate(octocat);
+    // append resulting html to the placeholder area
+    $("#octocats-placeholder").append(octocatHtml);
+  });
+});
---
+++ data.js
@@ -0,0 +1,86 @@
+var data = {
+  school: "General Assembly",
+  city: "San Francisco",
+  course: "Web Development Immersive",
+  course_id: "WDI21",
+  classroom: "6",
+  octocats: [
+    {
+      id: 0,
+      lastName: "Farquhar",
+      firstName: "Bradley",
+      githubUsername: "farquhar86",
+      image: "https://octodex.github.com/images/spidertocat.png"
+    },
+    {
+      id: 1,
+      lastName: "Preys",
+      firstName: "Daniel",
+      githubUsername: "danielpreys",
+      image: "https://octodex.github.com/images/stormtroopocat.png"
+    },
+    {
+      id: 2,
+      lastName: "Monaghan",
+      firstName: "Darragh",
+      githubUsername: "darraghmonaghan",
+      image: "https://octodex.github.com/images/pusheencat.png"
+    },
+    {
+      id: 3,
+      lastName: "Euresti",
+      firstName: "Eric",
+      githubUsername: "eeuresti",
+      image: "https://octodex.github.com/images/murakamicat.png"
+    },
+    {
+      id: 4,
+      lastName: "Serpa",
+      firstName: "Francis",
+      githubUsername: "FCSerpa",
+      image: "https://octodex.github.com/images/heisencat.png"
+    },
+    {
+      id: 5,
+      lastName: "Stenson",
+      firstName: "Heather",
+      githubUsername: "HeyStenson",
+      image: "https://octodex.github.com/images/twenty-percent-cooler-octocat.png"
+    },
+    {
+      id: 6,
+      lastName: "Calero",
+      firstName: "Humberto",
+      githubUsername: "altairn5",
+      image: "https://octodex.github.com/images/codercat.jpg"
+    },
+    {
+      id: 7,
+      lastName: "Nei",
+      firstName: "Ian",
+      githubUsername: "inei",
+      image: "https://octodex.github.com/images/wilson.jpg"
+    },
+    {
+      id: 8,
+      lastName: "Bennington",
+      firstName: "James",
+      githubUsername: "jkbennington",
+      image: "https://octodex.github.com/images/visionary.jpg"
+    },
+    {
+      id: 9,
+      lastName: "Evangelista",
+      firstName: "Jamie",
+      githubUsername: "jevangelista",
+      image: "https://octodex.github.com/images/Professortocat_v2.png"
+    },
+    {
+      id: 10,
+      lastName: "Almeida",
+      firstName: "Justin",
+      githubUsername: "jhalmeida",
+      image: "https://octodex.github.com/images/yaktocat.png"
+    }
+  ]
+}
